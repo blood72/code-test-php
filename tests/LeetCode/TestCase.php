be5b179d9628ce9cec8764c78222568d4fa855e1
blood72 <blood72@kakao.com>
2021-06-26 20:06:33 +0900

    ✨ Add LeetCode base test case

---
+++ tests/LeetCode/TestCase.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\LeetCode;
+
+use LeetCode\SolutionInterface;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    protected SolutionInterface $solution;
+    protected array $data = [];
+
+    /** @dataProvider dataProvider */
+    abstract public function test($actual, $expect): void;
+
+    public function dataProvider(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Dynamically execute the first method of the solution class.
+     *
+     * @return mixed
+     */
+    protected function solve(): mixed
+    {
+        $method = get_class_methods($this->solution)[0];
+
+        return $this->solution->$method(...func_get_args());
+    }
+
+    /**
+     * Guess the appropriate solution class for the test.
+     *
+     * @return string
+     */
+    protected function guessSolutionClass(): string
+    {
+        // ex. "Tests"(5) . "\LeetCode\Array101\MaxConsecutiveOnes" . "Test"(4)
+        $basename = substr(get_class($this), 5, -4);
+
+        return $basename . '\\Solution';
+    }
+
+    /**
+     * This method is called before each test.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (class_exists($this->guessSolutionClass())) {
+            $class = $this->guessSolutionClass();
+            $this->solution = new $class;
+        }
+    }
+}
